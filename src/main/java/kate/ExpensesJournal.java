9d9522e4c07664e657674696a49094c25fa3a509
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-08-19 21:00:45 +0300

    Expenses journal

--- src/main/java/kate/ExpensesJournal.java
+++ src/main/java/kate/ExpensesJournal.java
@@ -12,14 +12,13 @@ import java.util.*;
 
 public class ExpensesJournal extends HttpServlet {
     private long sum = 0;
-    private List<LocalDate> localDateList = new ArrayList<>();
-    private List<Integer> integerList = new ArrayList<>();
+    private Map<LocalDate, Integer> expensesByDate = new HashMap<>();
 
     /**
      * The expenses journal should save all numbers and sum them as a total amount of expenses.
      * The expenses journal should show the journal history.
      * Possible endpoints:  /expenses-journal?operation=addRecord&date=2022-08-18&amount=120
-     *                      /expenses-journal?operation=showHistory
+     * /expenses-journal?operation=showHistory
      *
      * @param req  date, number
      * @param resp
@@ -34,33 +33,50 @@ public class ExpensesJournal extends HttpServlet {
                 .append("<title>Expenses journal</title></head><body>");
 
         if ("addRecord".equals(operation)) {
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");//may be excluded, because datetimeformatter is default
             String dateString = req.getParameter("date");
             LocalDate localDate = LocalDate.parse(dateString, dateTimeFormatter);
             int amount = Integer.parseInt(req.getParameter("amount"));
-            integerList.add(amount);
-            localDateList.add(localDate);
-            sum += amount;
-            htmlResponse.append("<h3>The total expenses sum for now is " + sum + "</h3>")
-                    .append("<p>The amount is " + amount + "</p>")
-                    .append("<p>The date is " + localDate + "</p></html>");
-            resp.getWriter().println(htmlResponse);
+            htmlResponse.append(addRecord(localDate, amount));
         }
 
         if ("showHistory".equals(operation)) {
-            htmlResponse.append("<h2>Expenses Journal</h2><table border=\"1\"><tr><th>Amount</th><th>Date</th></tr>");
-            for (int a = 0; a < integerList.size(); a++) {
-                htmlResponse.append("<tr>");
-                htmlResponse.append("<td>" + integerList.get(a) + "</td>");
-                for (int d = a; d < localDateList.size(); d++) {
-                    htmlResponse.append("<td>" + localDateList.get(d) + "</td>");
-                    break;
-                }
-                htmlResponse.append("</tr>");
-            }
-            htmlResponse.append("</table>");
-            resp.getWriter().println(htmlResponse);
+            htmlResponse.append(showHistory());
         }
         htmlResponse.append("</body></html>");
+        resp.getWriter().println(htmlResponse);
+    }
+
+    private String addRecord(LocalDate date, int amount) {
+        // Alternative to Map.compute
+//        if(expensesByDate.containsKey(date)){
+//            int newAmonut = expensesByDate.get(date) + amount;
+//            expensesByDate.put(date,newAmonut);
+//        } else {
+//            expensesByDate.put(date,amount);
+//        }
+        expensesByDate.compute(date, (key, oldValue) -> oldValue == null ? amount : amount + oldValue);// threadsafe in case of concurrent HM
+
+        sum += amount;
+
+        return "<h3>The total expenses sum for now is " + sum + "</h3>" +
+                "<p>The amount is " + amount + "</p>" +
+                "<p>The date is " + date + "</p></html>";
+    }
+
+    private String showHistory() {
+        StringBuilder htmlResponse = new StringBuilder();
+        htmlResponse.append("<h2>Expenses Journal</h2><table border=\"1\"><tr><th>Amount</th><th>Date</th></tr>");
+        for (Map.Entry<LocalDate, Integer> e : expensesByDate.entrySet()) {
+            int expense = e.getValue();
+            LocalDate date = e.getKey();
+
+            htmlResponse.append("<tr>");
+            htmlResponse.append("<td>").append(expense).append("</td>");
+            htmlResponse.append("<td>").append(date).append("</td>");
+            htmlResponse.append("</tr>");
+        }
+        htmlResponse.append("</table>");
+        return htmlResponse.toString();
     }
 }
